a046c3e8839d544afae90f9d28c5d57f31674691
gayanar02 <gayanar054@gmail.com>
2026-07-22 12:49:31 +0530

    let keyword

--- day/day 8/p4.js
+++ day/day 8/p4.js
@@ -10,3 +10,16 @@ if(true)
 }
 // var in JS is not a block-scoped
 console.log(message)
+
+//**************************************************************************************************************************************
+// "let" keyword based variable
+let score = "272";
+// let score = "100"; //SyntaxError: Identifier 'score' has already been declared( cant create another one)
+score ="2702" // re-assignment is valid
+console.log(score)
+
+if(true)
+{
+    let secret = "272411@:)"; // "let" in JS is block-scoped
+}
+console.log(secret)
