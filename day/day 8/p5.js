7516bd4ede7959f49943b9d08b87071945025f7b
gayanar02 <gayanar054@gmail.com>
2026-07-22 13:25:03 +0530

    data types in JS

---
+++ day/day 8/p5.js
@@ -0,0 +1,14 @@
+// Primitive data types
+// 1. String: used for Text
+// 2. Number: integers/ decimal 
+// 3. Bigint: very large integers
+// 4. Boolean: true/false
+// 5. Undefined: variable is declared but not assigned
+// 6. Null: represents an intentional empty value
+// 7. Symbol: to create unique identifiers
+
+// Non-primitive data types : are more complex and stored by references
+// 1. Object: to store collections of key-value pairs
+// 2. Array: special type of object for storing ordered lists
+// 3. Function: are also considered as objects
+
