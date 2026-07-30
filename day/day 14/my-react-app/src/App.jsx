9e77dd12eb0cd39fd722d9aa3274e7d60cabafe0
gayanar02 <gayanar054@gmail.com>
2026-07-30 15:54:57 +0530

    form three

--- day/day 14/my-react-app/src/App.jsx
+++ day/day 14/my-react-app/src/App.jsx
@@ -7,6 +7,7 @@ import UserListHook from "./components/HookTwo";
 import ThemedProviderDemo from "./components/HookThree";
 import SimpleNameForm from "./components/FormOne";
 import SignupForm from "./components/FormTwo";
+import ValidatedForm from "./components/FormThree";
 function App() {
   
 
@@ -20,6 +21,7 @@ function App() {
       { /*<ThemedProviderDemo/>*/}
       <SimpleNameForm />
       <SignupForm />
+      <ValidatedForm />
     </>
   )
 }
